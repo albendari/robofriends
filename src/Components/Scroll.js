ea48000a89e39089adeaa2f9f9da07e22017e5a8
albendari fawaz <albendarifawaz@albendaris-MacBook-2.local>
2020-05-27 22:49:44 +0300

    robotfrind project

---
+++ src/Components/Scroll.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Scroll = (props) =>{
+	return (
+		<div style={{overflowY: 'scroll', border:'5px solid white',height:'500px'}}>
+			{props.children}
+		</div>
+
+		);
+};
+
+export default Scroll;
